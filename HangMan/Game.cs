907dd47189bc2535c3102f47082a653be77596e0
seedback <seedback2@gmail.com>
2022-03-16 14:49:03 +0100

    Initial commit

---
+++ HangMan/Game.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan
+{
+    internal class Game
+    {
+        static Game()
+        {
+            StageList.Add((string)
+                          "     ____\n" +
+                          "    |/\n" +
+                          "    |\n" +
+                          "    |\n" +
+                          "    |\n" +
+                          "    |\n" +
+                          "   /|\\\n" +
+                          "__/_|_\\______\n"
+            );
+            StageList.Add((string)
+                          "     ____\n" +
+                          "    |/   |\n" +
+                          "    |\n" +
+                          "    |\n" +
+                          "    |\n" +
+                          "    |\n" +
+                          "   /|\\\n" +
+                          "__/_|_\\______\n"
+            );
+            StageList.Add((string)
+                          "     ____\n" +
+                          "    |/   |\n" +
+                          "    |   (_)\n" +
+                          "    |\n" +
+                          "    |\n" +
+                          "    |\n" +
+                          "   /|\\\n" +
+                          "__/_|_\\______\n"
+            );
+            StageList.Add((string)
+                          "     ____\n" +
+                          "    |/   |\n" +
+                          "    |   (_)\n" +
+                          "    |    |\n" +
+                          "    |\n" +
+                          "    |\n" +
+                          "   /|\\\n" +
+                          "__/_|_\\______\n"
+            );
+            StageList.Add((string)
+                          "     ____\n" +
+                          "    |/   |\n" +
+                          "    |   (_)\n" +
+                          "    |   /|\n" +
+                          "    |\n" +
+                          "    |\n" +
+                          "   /|\\\n" +
+                          "__/_|_\\______\n"
+            );
+            StageList.Add((string)
+                          "     ____\n" +
+                          "    |/   |\n" +
+                          "    |   (_)\n" +
+                          "    |   /|\\\n" +
+                          "    |\n" +
+                          "    |\n" +
+                          "   /|\\\n" +
+                          "__/_|_\\______\n"
+            );
+            StageList.Add((string)
+                          "     ____\n" +
+                          "    |/   |\n" +
+                          "    |   (_)\n" +
+                          "    |   /|\\\n" +
+                          "    |    |\n" +
+                          "    |\n" +
+                          "   /|\\\n" +
+                          "__/_|_\\______\n"
+            );
+            StageList.Add((string)
+                          "     ____\n" +
+                          "    |/   |\n" +
+                          "    |   (_)\n" +
+                          "    |   /|\\\n" +
+                          "    |    |\n" +
+                          "    |   /\n" +
+                          "   /|\\\n" +
+                          "__/_|_\\______\n"
+            );
+            StageList.Add((string)
+                          "     ____\n" +
+                          "    |/   |\n" +
+                          "    |   (_)\n" +
+                          "    |   /|\\\n" +
+                          "    |    |\n" +
+                          "    |   / \\\n" +
+                          "   /|\\\n" +
+                          "__/_|_\\______\n"
+            );
+        }
+
+        public static void Setup()
+        {
+            Word = ChooseWord().ToUpper();
+
+            foreach (char c in Word)
+            {
+                if (c == ' ')
+                {
+                    ShownWord = ShownWord.Insert(ShownWord.Length, " ");
+                    continue;
+                }
+                ShownWord = ShownWord.Insert(ShownWord.Length, "_");
+            }
+            UsedLetters = "";
+        }
+
+        public static bool PrintStage(int stage)
+        {
+            Console.Clear();
+            if (stage >= 0 && stage < StageList.Count-1)
+            {
+                Console.WriteLine(StageList[stage]);
+                return true;
+            }
+            else if(stage < StageList.Count)
+            {
+                Console.WriteLine(StageList[stage]);
+            }
+
+            return false;
+        }
+
+        public static bool CheckLetterIsNew(char letter)
+        {
+            letter = Convert.ToChar(Convert.ToString(letter).ToUpper());
+            foreach (char usedLetter in UsedLetters)
+            {
+                if (usedLetter == letter)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool CheckLetter(char letter)
+        {
+            bool isIncluded = false;
+            letter = Convert.ToChar(Convert.ToString(letter).ToUpper());
+            UsedLetters = UsedLetters.Insert(UsedLetters.Length, Convert.ToString(letter));
+            for (int i = 0; i < Word.Length; i++)
+            {
+                if (Word[i] == letter)
+                {
+                    ShownWord = ShownWord.Remove(i, 1);
+                    ShownWord = ShownWord.Insert(i, Convert.ToString(letter));
+                    isIncluded = true;
+                }
+            }
+            return isIncluded;
+        }
+
+
+        // TODO: (Seedback) Add function to replace æøå with \u00E6 \u00F8 \u00E5 
+        public static string ChooseWord()
+        {
+            var lines = File.ReadAllLines(@"D:\Documents\School\GetAcademy\StartitM3Oppgaver\HangMan\HangMan\words.txt");
+            var r = new Random();
+            var randomLineNumber = r.Next(0, lines.Length - 1);
+            var line = lines[randomLineNumber];
+            return line;
+        }
+
+        public static void PrintBoard(int i)
+        {
+            if (i == -1)
+            {
+                i = StageList.Count-1;
+            }
+
+            Console.Clear();
+            PrintStage(i);
+            //Console.WriteLine(Game.Word);
+            Console.WriteLine(Game.ShownWord);
+            Console.WriteLine(Game.UsedLetters);
+        }
+
+        public static List<string> StageList = new List<string>();
+
+        public static string Word = "";
+        public static string ShownWord = "";
+        public static string UsedLetters = "";
+    }
+}
---
+++ HangMan/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Channels;
+
+
+namespace HangMan
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Game.Setup();
+
+                for (int i = 0; Game.PrintStage(i); i++)
+                {
+                    Console.OutputEncoding = Encoding.UTF8;
+                    Game.PrintBoard(i);
+
+                    bool isFinished = false;
+                    string input = "";
+
+                    while (!isFinished)
+                    {
+                        Game.PrintBoard(i);
+                        input = Console.ReadLine();
+                        if (input == String.Empty)
+                        {
+                            continue;
+                        }
+
+                        isFinished = Game.CheckLetterIsNew(input[0]);
+                    }
+
+                    if (Game.CheckLetter(input[0]))
+                    {
+                        i--;
+                    }
+
+                    if (Game.Word == Game.ShownWord)
+                    {
+                        break;
+                    }
+                }
+
+                Game.PrintBoard(-1);
+                Console.WriteLine();
+
+                if (Game.Word == Game.ShownWord)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("That is correct!");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write("You lost!");
+                }
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.ReadLine();
+
+                Console.Clear();
+                Console.WriteLine("Do you want to play again? (y/n)");
+                string quitAnswer;
+                while (true)
+                {
+                    quitAnswer = Console.ReadLine() ?? String.Empty;
+                    if (quitAnswer.ToUpper() == "Y" || quitAnswer.ToUpper() == "N")
+                    {
+                        break;
+                    }
+                }
+
+                if (quitAnswer.ToUpper() == "N")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
